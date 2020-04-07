124d9e41fd023e515aa5ab72fd3c417724638b2d
Pedro Augusto <pedro.augusto@osf.digital>
2020-04-06 21:43:56 -0300

    creating react components
    creating class components
    simple routing (without react-router)

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>about page</h1>
+        <p>this app use react</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <a href="/">home</a> | <a href="/about">about</a>
+    </nav>
+  );
+}
+
+export default Header;
